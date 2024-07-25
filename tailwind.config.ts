aeae6f799f26fb780a9bc1a12167ed0aa3f548c4
nobletp0001 <machine@machines-MacBook-Pro.local>
2024-07-25 08:33:28 +0100

    dashboard

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,39 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        btnbg: '#CC399E',
+        bggrey:"#EDF2F7",
+        bg1:"rgba(255, 237, 249, 0.1)",
+        bg2:"rgba(255, 237, 249, 0.2)",
+        bg3:"rgba(255, 237, 249, 0.3)",
+        black:"#101725",
+        bgd:"#E2E8F0",
+        textblack:"#4D2259",
+        iconcolor:"#A0AEC0",
+        lightwhite:"#FAFAFA",
+        lightblack:"#27303F",
+        lighterblack:"#27303F",
+        lightgreen:"#A0AEC0",
+        lightergreen:"#718096"
+      },
+      fontSize: {
+        'f32': '32px',
+        'f43': '43px',
+      },
+      fontFamily:{
+        "fpublic":"Public Sans",
+        "circular": ['CircularStd'],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
 };
+
 export default config;
